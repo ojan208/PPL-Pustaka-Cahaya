ffd3f8386ce24586bd188fab50cd211a37598f13
ojan208 <mfauzanazhima18@gmail.com>
2024-05-13 07:19:15 +0700

    API login V1

--- Back-end/controllers/auth_controller.go
+++ Back-end/controllers/auth_controller.go
@@ -1,21 +1,38 @@
 package controllers
 
 import (
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 	"encoding/json"
 	"net/http"
 	"ppl/models"
 	"ppl/helper"
 	"github.com/go-playground/validator/v10"
-	"fmt"
+	"strconv"
+	"time"
 )
+
+func GenerateToken(userID int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(1 * time.Hour).Unix(),
+		IssuedAt:  time.Now().Unix(),
+		Subject:   strconv.Itoa(userID),
+	})
+
+	signedToken, err := token.SignedString(helper.SecretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
 
 func RegisterController(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	validate := validator.New()
 
 	err := json.NewDecoder(r.Body).Decode(&user)
-	fmt.Printf("%+v\n", user)
 
 	err = validate.Struct(user)
 	if err != nil {
@@ -49,7 +66,6 @@ func RegisterController(w http.ResponseWriter, r *http.Request) {
 	}
 	user.Password = string(hashedPassword)
 
-	fmt.Printf("%+v\n", user)
 	_, err = DB.Exec("INSERT INTO user (nama, email, password) VALUES (?, ?, ?)", 
 					user.Nama, user.Email, user.Password)
 	if err != nil {
@@ -93,11 +109,44 @@ func RegisterControllerOAuth(w http.ResponseWriter, r *http.Request) {
 
 func LoginController(w http.ResponseWriter, r *http.Request) {
 	// ambil value dari body request
-	// hash password yang diterima
-	// query untuk melihat validasi
-	// if valid, send token
-	// if not, send warning
-	// error checking every step of the way
+	var user models.User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		response := models.NewErrorResponse("Invalid Request Payload", "Bad Request", err.Error())
+		helper.WriteToResponseBody(w, http.StatusBadRequest, &response)
+		return
+	}
+
+	var userDB models.User
+	err = DB.QueryRow("SELECT userID, email, password FROM user WHERE email = ?", user.Email).Scan(&userDB.UserID, &userDB.Email, &userDB.Password)
+	if err != nil {
+		response := models.NewErrorResponse("Login Failed", "Unauthorized", "Invalid Email")
+		helper.WriteToResponseBody(w, http.StatusUnauthorized, &response)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(userDB.Password), []byte(user.Password))
+	if err != nil {
+		response := models.NewErrorResponse("Login Failed", "Unauthorized", "Invalid Password")
+		helper.WriteToResponseBody(w, http.StatusUnauthorized, &response)
+		return
+	}
+
+	token, err := GenerateToken(userDB.UserID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	responseData := models.UserLogin{
+		UserID: strconv.Itoa(userDB.UserID),
+		Email: userDB.Email, 
+		Token: token,
+	}
+
+	response := models.NewSuccessResponse("Login successful", responseData)
+	helper.WriteToResponseBody(w, http.StatusOK, response)
 }
 
 func LoginControllerOAuth(w http.ResponseWriter, r *http.Request) {
