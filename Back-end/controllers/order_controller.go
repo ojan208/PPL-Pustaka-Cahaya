689cf9eff8d017586a12d7d279f9e2123f7351a0
ojan208 <mfauzanazhima18@gmail.com>
2024-06-12 08:40:34 +0700

    BACKEND DONE LETSGOOOOOO

--- Back-end/controllers/order_controller.go
+++ Back-end/controllers/order_controller.go
@@ -9,9 +9,12 @@ import (
 	"bytes"
 	"encoding/base64"
 	"io"
+	"crypto/sha512"
+	"encoding/hex"
+	// "time"
 	// "github.com/gorilla/mux"
 	"strconv"
-	// "fmt"
+	"fmt"
 )
 
 func MakeOrderToken(w http.ResponseWriter, r *http.Request) {
@@ -41,7 +44,9 @@ func MakeOrderToken(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusNotFound)
 			return
 		}	
-		totalBookPrice += bookTemp.Harga - (bookTemp.Harga * bookTemp.Diskon)
+		totalBookPrice += bookTemp.Harga - (bookTemp.Harga * bookTemp.Diskon / 100)
+		// fmt.Println(bookTemp.Harga - (bookTemp.Harga * bookTemp.Diskon))
+		// fmt.Println(totalBookPrice)
 		bookItems = append(bookItems, bookTemp)
 	}
 	if noTelp.Valid {
@@ -81,6 +86,7 @@ func MakeOrderToken(w http.ResponseWriter, r *http.Request) {
 	var transReq models.TransactionInfo
 	transReq.OrderID = strconv.Itoa(int(orderId))
 	transReq.GrossAmount = finalPrice
+	// fmt.Println(transReq.GrossAmount)
 
 	var userReq models.UserInfo
 	userReq.Nama = user.Nama
@@ -100,6 +106,9 @@ func MakeOrderToken(w http.ResponseWriter, r *http.Request) {
 			temp.Judul = book.Judul
 		}
 		items = append(items, temp)
+		if (book.Diskon > 0) {
+			items = append(items, models.ItemInfo{BukuID: "0", Harga: -1 * (book.Harga * book.Diskon / 100), Jumlah: 1, Judul: "Diskon"})
+		}
 	}
 
 	var final models.Payload
@@ -108,6 +117,7 @@ func MakeOrderToken(w http.ResponseWriter, r *http.Request) {
 	final.Transaction = transReq
 	
 	payload, err := json.Marshal(final)
+	// fmt.Println(string(payload))
 
 	tokenReq, _ := http.NewRequest("POST", midtransUrl, bytes.NewReader(payload))
 
@@ -130,4 +140,101 @@ func MakeOrderToken(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	res := models.NewSuccessResponse("Berhasil membuat order", string(body))
 	helper.WriteToResponseBody(w, http.StatusOK, res)
+}
+
+type MidtransReq struct {
+	OrderId				string		`json:"order_id"`			
+	SignatureKey		string		`json:"signature_key"`
+	StatusCode			string		`json:"status_code"`
+	TransactionStatus	string		`json:"transaction_status"`
+	FraudStatus			string		`json:"fraud_status"`
+	GrossAmount			string		`json:"gross_amount"`
+}
+
+func handleAfterPayment(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hai Midtrans :)")
+	// time.Sleep(120 * time.Second) 
+	var receivedReq MidtransReq
+	err := json.NewDecoder(r.Body).Decode(&receivedReq)
+	if err != nil {
+		// Informasi akun tidak sesuai ekspektasi (Password < 8 char, tidak mengisi email/nama)
+		response := models.NewErrorResponse("Bad Request", "Informasi Akun tidak sesuai dengan ketentuan", err.Error())
+		helper.WriteToResponseBody(w, http.StatusBadRequest, &response)
+		return
+	}
+	fmt.Println("Hai Midtrans :)")
+	// orderId, err := strconv.Atoi(receivedReq.OrderId)
+	// var GrossAmount int
+	// var userID int
+	fmt.Println(receivedReq)
+	// rows, err := DB.Query("SELECT totalHarga, userID FROM orders WHERE orderId = ?", orderId)
+	// if rows.Next() {
+	// 	err := rows.Scan(&GrossAmount, &userID)
+	// 	if err != nil {
+	// 		http.Error(w, err.Error(), http.StatusNotFound)
+	// 		return
+	// 	}	
+	// }
+
+	serverKey := "SB-Mid-server-jVdnOj1gvosN0zYrCXDmNTZC"
+	// sha_512 := sha512.New()
+	hash := sha512.Sum512([]byte(receivedReq.OrderId+receivedReq.StatusCode+receivedReq.GrossAmount+serverKey))
+	hashedLocal := hash[:]
+	// signatureKeyHash = sha512.Sum512([receivedReq.SignatureKey])
+	receivedHashBytes, err := hex.DecodeString(receivedReq.SignatureKey)
+	if err != nil {
+		// Ada kesalahan dengan koneksi db
+		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(bytes.Equal(hashedLocal, receivedHashBytes))
+	if (bytes.Equal(hash[:], receivedHashBytes)) {
+		fmt.Println("Duar masuk if")
+		statusPembayaran := ""
+		if (receivedReq.TransactionStatus == "capture"){
+			if (receivedReq.FraudStatus == "accept"){
+				response := models.NewSuccessResponseNoData("Status Pembayaran Berhasil Diverifikasi")
+				helper.WriteToResponseBody(w, http.StatusCreated, &response)
+				statusPembayaran = "PAYMENT SUCCESS"
+				fmt.Println(statusPembayaran)
+				// return
+			}
+		} else if (receivedReq.TransactionStatus == "settlement"){
+			response := models.NewSuccessResponseNoData("Status Pembayaran Berhasil Diverifikasi")
+			helper.WriteToResponseBody(w, http.StatusCreated, &response)
+			statusPembayaran = "SETTLEMENT"
+			fmt.Println(statusPembayaran)
+			// return
+		} else if (receivedReq.TransactionStatus == "cancel" ||
+			receivedReq.TransactionStatus == "deny" ||
+			receivedReq.TransactionStatus == "expire"){
+			response := models.NewSuccessResponseNoData("Status Pembayaran Berhasil Diverifikasi")
+			helper.WriteToResponseBody(w, http.StatusCreated, &response)
+			statusPembayaran = "GAGAL DIBAYAR"
+			fmt.Println(statusPembayaran)
+			// return
+		} else if (receivedReq.TransactionStatus == "pending"){
+			response := models.NewSuccessResponseNoData("Status Pembayaran Berhasil Diverifikasi")
+			helper.WriteToResponseBody(w, http.StatusCreated, &response)
+			statusPembayaran = "PENDING"
+			fmt.Println(statusPembayaran)
+			// return
+		}
+		_, err = DB.Exec("UPDATE orders SET konfirmasi = ? WHERE orderid = ?", statusPembayaran, receivedReq.OrderId)
+		if err != nil {
+			// Ada kesalahan dengan koneksi db
+			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		return;
+		
+	} else {
+		response := models.NewErrorResponse("500", "Gagal Verifikasi Pembayaran", "Invalid Signature Key")
+		helper.WriteToResponseBody(w, http.StatusUnauthorized, &response)
+		return
+	}
+	// sha_512.Sum(nil)
 }
