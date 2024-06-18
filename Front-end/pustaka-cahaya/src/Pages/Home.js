64b0df436f92c242c0f0525a5d2f7775239e8ebb
itscatus <felicianatania11@gmail.com>
2024-06-18 13:29:47 +0700

    revised homepage

--- Front-end/pustaka-cahaya/src/Pages/Home.js
+++ Front-end/pustaka-cahaya/src/Pages/Home.js
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/swiper-bundle.css';
 import { Autoplay } from "swiper/modules";
 
-const Home = () => {
+const Home = (props) => {
     const swiperOptionsOne={
         breakpoints: {
             0:{
@@ -28,9 +28,65 @@ const Home = () => {
                     <div className="content">
                         <h3>New Arrival</h3>
                         <p>Kumpulan buku-buku yang baru diterbitkan.</p>
-                        <a href="\#" className="btn">Beli Sekarang</a>
+                        <a href="/products" className="btn">Beli Sekarang</a>
                     </div>
+                    
                     <div className="swiper">
+                        <h3>Best Seller</h3>
+                        <Swiper 
+                            watchSlidesProgress={true}
+                            slidesPerView={5}
+                            autoplay={{
+                                delay:9500,
+                                disableOnInteraction:false,
+                            }}
+                            pagination={{clickable:true}}
+                            modules={[Autoplay]}
+                            className="book-list"
+                            {...swiperOptionsOne}>
+                            <a href="\#" className='swiper-slide'>
+                            <SwiperSlide>
+                            <a href="\#" className='swiper-slide'>
+                            <SwiperSlide><img src="./images/Laut-Bercerita.jpg" alt="laut-bercerita"></img></SwiperSlide>
+                            </a>
+                            </SwiperSlide>
+                            </a>
+                            <a href="\#" className='swiper-slide'>
+                            <SwiperSlide><img src="./images/Laut-Bercerita.jpg" alt="laut-bercerita"></img></SwiperSlide>
+                            </a>
+                            <a href="\#" className='swiper-slide'>
+                            <SwiperSlide><img src="./images/fucek.jpg" alt="fucek"></img></SwiperSlide>
+                            </a>
+                        </Swiper>
+                    </div>
+
+                    <div className="swiper">
+                        <h3>Promo Hari Ini</h3>
+                        <Swiper 
+                            watchSlidesProgress={true}
+                            slidesPerView={5}
+                            autoplay={{
+                                delay:9500,
+                                disableOnInteraction:false,
+                            }}
+                            pagination={{clickable:true}}
+                            modules={[Autoplay]}
+                            className="book-list"
+                            {...swiperOptionsOne}>
+                            <a href="\#" className='swiper-slide'>
+                            <SwiperSlide><img src="./images/sagaras.jpeg" alt="sagaras"></img></SwiperSlide>
+                            </a>
+                            <a href="\#" className='swiper-slide'>
+                            <SwiperSlide><img src="./images/Laut-Bercerita.jpg" alt="laut-bercerita"></img></SwiperSlide>
+                            </a>
+                            <a href="\#" className='swiper-slide'>
+                            <SwiperSlide><img src="./images/fucek.jpg" alt="fucek"></img></SwiperSlide>
+                            </a>
+                        </Swiper>
+                    </div>
+
+                    <div className="swiper">
+                        <h3>Fantasi Terbaik</h3>
                         <Swiper 
                             watchSlidesProgress={true}
                             slidesPerView={5}
