1dd26d637b8ab8e3870b18a178bf719fbec68cfc
ojan208 <mfauzanazhima18@gmail.com>
2024-06-18 17:42:54 +0700

    pake dotenv jadinya

--- Front-end/pustaka-cahaya/src/Components/Navbar.jsx
+++ Front-end/pustaka-cahaya/src/Components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:8080/auth/login', {
+            const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/auth/login', {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
