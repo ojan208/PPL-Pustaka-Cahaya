99574687ec41c708e8e8d78eefeb1ca9820321cf
itscatus <felicianatania11@gmail.com>
2024-06-18 21:04:33 +0700

    added not-found page

--- Front-end/pustaka-cahaya/src/Pages/Profile.jsx
+++ Front-end/pustaka-cahaya/src/Pages/Profile.jsx
@@ -3,21 +3,19 @@ import Footer from "../Components/Footer";
 
 const Profile = () => {
     return (
-        <div>
+        <div className="profile-container">
             <Navbar/>
+            <div className="row">
+                <div>Profil</div>
+                <div>Alamat</div>
+            </div>
             <div className="profile-form-container">
                 <form onSubmit={"#"}>
                     {/* pokoknya tampilin buat si kolom kolomnya tuh udah keisi dluan */}
                     <input type="text" className="box" placeholder="Nama Lengkap" required />
                     <input type="email" className="box" placeholder="Email" required />
                     <input type="password" className="box" placeholder="Kata Sandi" required />
-                    {/* <div className="checkbox">   */}
-                        <input type="checkbox" id="consent"/>
-                        <label>Dengan pembuatan akun, Anda menyetujui Syarat & Ketentuan serta Kebijakan Privasi kami</label>
-                    {/* </div> */}
-                    {/* <form> */}
-                    <input type="submit" value="Daftar" className="btn"/>
-                    {/* </form> */}
+                    <input type="submit" value="Simpan" className="btn"/>
                 </form>
             </div>
             <Footer/>
